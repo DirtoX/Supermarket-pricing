6412f3f01e56888837735b6717854e3b709fec0e
DirtoX <youcefberghida@gmail.com>
2021-12-05 23:34:44 +0100

    Add price to Product Product price test success

--- src/main/java/com/github/dirtox/supermarket/models/Product.java
+++ src/main/java/com/github/dirtox/supermarket/models/Product.java
@@ -3,8 +3,11 @@ package com.github.dirtox.supermarket.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 public class Product {
     private String name;
+    private BigDecimal price;
 }
--- src/test/java/com/github/dirtox/supermarket/ProductTest.java
+++ src/test/java/com/github/dirtox/supermarket/ProductTest.java
@@ -3,18 +3,21 @@ package com.github.dirtox.supermarket;
 import com.github.dirtox.supermarket.models.Product;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ProductTest {
     @Test
     public void product_should_have_a_name(){
-        Product product = new Product("Product 1");
+        Product product = new Product("Product 1", new BigDecimal(5));
         assertEquals(product.getName(), "Product 1");
     }
 
     @Test
     public void product_should_have_a_price(){
-        Product product = new Product("Product 1");
-        assertEquals(product.getPrice(), "Product 1");
+        Product product = new Product("Product 1", new BigDecimal(5));
+        assertTrue(product.getPrice().compareTo(new BigDecimal(5)) == 0);
     }
 }
