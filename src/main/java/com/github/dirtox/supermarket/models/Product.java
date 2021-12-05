42f60c7ab21623e15f60f2fc1bc6b042e1c0bbd6
DirtoX <youcefberghida@gmail.com>
2021-12-05 23:28:47 +0100

    add name to Product class Product name test success

--- src/main/java/com/github/dirtox/supermarket/models/Product.java
+++ src/main/java/com/github/dirtox/supermarket/models/Product.java
@@ -1,4 +1,10 @@
 package com.github.dirtox.supermarket.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Product {
+    private String name;
 }
