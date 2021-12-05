8846dc33720a6e83fe8985ae9d3b36ee1ea78ee5
DirtoX <youcefberghida@gmail.com>
2021-12-06 00:01:28 +0100

    add Product to CartItem add Quantity to CartItem CartItem quantity test success

--- src/main/java/com/github/dirtox/supermarket/models/CartItem.java
+++ src/main/java/com/github/dirtox/supermarket/models/CartItem.java
@@ -1,4 +1,13 @@
 package com.github.dirtox.supermarket.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
 public class CartItem {
+    private final Product product;
+    private final BigDecimal quantity;
 }
--- src/test/java/com/github/dirtox/supermarket/CartItemTest.java
+++ src/test/java/com/github/dirtox/supermarket/CartItemTest.java
@@ -6,11 +6,14 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CartItemTest {
 
     @Test
     public void cart_item_should_have_product_quantity(){
         Product product = new Product("product1",new BigDecimal(50.5));
         CartItem cartItem = new CartItem(product, new BigDecimal(5));
+        assertTrue(cartItem.getQuantity().compareTo(new BigDecimal(5)) == 0);
     }
 }
