37e0451e1cafcab83568bfe3d35a4601e1b95a1c
DirtoX <youcefberghida@gmail.com>
2021-12-06 00:55:34 +0100

    Cart duplicate items test failed

--- src/test/java/com/github/dirtox/supermarket/CartTest.java
+++ src/test/java/com/github/dirtox/supermarket/CartTest.java
@@ -36,4 +36,15 @@ public class CartTest {
         assertEquals(cart.getItemsNumber(), 0);
     }
 
+    @Test
+    public void adding_duplicate_items_should_change_quantity(){
+        Product product1 = new Product("Product 1", new BigDecimal(5));
+        CartItem cartItem = new CartItem(product1, new BigDecimal(2));
+        Cart cart = new Cart();
+        cart.addItem(cartItem);
+        cart.addItem(cartItem);
+        assertEquals(cart.getItemsNumber(), 1);
+        assertTrue(cartItem.getQuantity().compareTo(new BigDecimal(4)) == 0);
+    }
+
 }
